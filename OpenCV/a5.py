784faa07fb3e4a23f664ace2a8b5a5f4e02cfc0e
mahesh0537 <mail2mahesh0537@gmail.com>
2021-01-26 22:48:52 +0530

    q5

--- OpenCV/a5.py
+++ OpenCV/a5.py
@@ -1,23 +1,23 @@
-import cv2 as cv
+import cv2 
 import numpy as np
 import time
 
-vid = cv.VideoCapture("vid.mp4")
+vid = cv2.VideoCapture("vid.mp4")
 
 while True:
     _ , img = vid.read()
-    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    img_gray = cv.GaussianBlur(img_gray, (3,3),0)
-    circles = cv.HoughCircles(img_gray,cv.HOUGH_GRADIENT,1,10,param1=50,param2=30,minRadius=0,maxRadius=30)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.GaussianBlur(img_gray, (3,3),0)
+    circles = cv2.HoughCircles(img_gray,cv2.HOUGH_GRADIENT,1,10,param1=50,param2=30,minRadius=0,maxRadius=30)
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for i in circles[0,:]:
             # draw the outer circle
-            cv.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
+            cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
 
 
-    cv.imshow("img",img)
-    cv.imshow("grey",img_gray)
-    if cv.waitKey(1) & 0xff == ord('q'):
+    cv2.imshow("img",img)
+    cv2.imshow("grey",img_gray)
+    if cv2.waitKey(1) & 0xff == ord('q'):
         break
     time.sleep(0.03)
