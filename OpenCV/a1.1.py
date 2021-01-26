3b7837ee70e1da79c0cfde46fe50d8d22f53198e
mahesh0537 <mail2mahesh0537@gmail.com>
2021-01-26 16:03:19 +0530

    resize

--- OpenCV/a1.1.py
+++ OpenCV/a1.1.py
@@ -1,6 +1,7 @@
 import cv2
 
 img = cv2.imread("image1.jpeg")
+img = cv2.resize(img, (400,300))
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 t, img_bw = cv2.threshold(img_gray,80,225, cv2.THRESH_BINARY)
 
